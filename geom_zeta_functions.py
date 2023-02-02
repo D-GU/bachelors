8df2631e8e2bd08b6c7a446a686d5313cc56a5b7
forest <dgodomg@gmail.com>
2023-02-02 15:19:51 +0300

    ;

--- geom_zeta_functions.py
+++ geom_zeta_functions.py
@@ -1,11 +1,12 @@
 import sympy as sp
+import complex as cp
 import numpy as np
 import sage
 
 
 class GeomZeta:
     def __init__(
-            self, a=None, b=None, c=None, A=None, B=None, C=None, P=None, Q=None, R=None, S=None
+            self, a=None, b=None, c=None, A=None, B=None, C=None, P=None, Q=None, R=None, S=None, Z=0
     ):
         self.a = a
         self.b = b
@@ -17,6 +18,14 @@ class GeomZeta:
         self.Q = Q
         self.R = R
         self.S = S
+        self.Z = Z
+
+    @property
+    def equidistant_construct(self):
+        eq = self.projection
+        if sp.factor(sp.sqf_norm(self.P - self.Q) - sp.sqf_norm(self.R - eq)) == 0:
+            return True
+        return False
 
     @property
     def collinear(self):
@@ -44,3 +53,72 @@ class GeomZeta:
         if sp.factor(sp.sqf_norm(self.P - self.Q) - sp.sqf_norm(self.R - self.S)) == 0:
             return True
         return False
+
+    @property
+    def perpendicular(self):
+        if sp.factor(self.a * sp.conjugate(self.b) + sp.conjugate(self.a) * self.b) == 0:
+            return True
+        return False
+
+    @property
+    def similar(self):
+        if sp.factor(
+                self.A * sp.conjugate(self.Q) +
+                self.B * sp.conjugate(self.R) +
+                self.C * sp.conjugate(self.P) -
+                self.B * sp.conjugate(self.P) -
+                self.C * sp.conjugate(self.Q) -
+                self.A * sp.conjugate(self.R)
+        ) == 0:
+            return True
+
+        return False
+
+    @property
+    def radical(self):
+        return sp.factor(
+            (self.Q * sp.conjugate(self.Q) -
+             self.Q * sp.conjugate(self.P) -
+             sp.conjugate(self.Q) * self.P -
+             self.S * sp.conjugate(self.S) +
+             self.S * sp.conjugate(self.R) +
+             sp.conjugate(self.S) * self.R -
+             sp.conjugate(self.P) * self.R +
+             self.P * sp.conjugate(self.R)) / 2 / sp.conjugate(self.R - self.P))
+
+    @property
+    def area(self):
+        return sp.factor(
+            ((sp.conjugate(self.B) - sp.conjugate(self.A)) *
+             (self.C - self.A) - (self.B - self.A) *
+             (sp.conjugate(self.C) - sp.conjugate(self.A))) / 4 / sp.I)
+
+    @property
+    def projection(self):
+        return sp.intersection(self.Z, sp.I * self.a, self.P, self.a)
+
+    @property
+    def center(self):
+        return sp.intersection(
+            (self.P + self.Q) / 2, sp.I * (self.P - self.Q),
+            (self.Q + self.R) / 2, sp.I * (self.Q - self.R))
+
+    @property
+    def centroid(self):
+        return sp.factor((self.A + self.B + self.C) / 3)
+
+    @property
+    def orthocenter(self):
+        return sp.intersection(self.A, sp.I * (self.B - self.C), self.B, sp.I * (self.A - self.C))
+
+    def symmetry(self, Z, P):
+        return sp.factor(2 * self.P - self.Z)
+
+    def reflection(self, Z, P, a):
+        return self.symmetry(self.Z, self.projection)
+
+    def second_point(self):
+        return self.reflection(self.P, self.Q, sp.I * self.a)
+
+    def scalar_product(self):
+        return sp.factor((self.a * sp.conjugate(self.b) + sp.conjugate(self.a) * self.b) / 2)
