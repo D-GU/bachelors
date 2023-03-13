58311517e247d51810b4e0b40597fc30be857769
forest <dgodomg@gmail.com>
2023-03-13 16:50:01 +0300

    ;

---
+++ cyclotomic_polynomial.py
@@ -0,0 +1,7 @@
+import numpy as np
+import complex as cp
+
+class CyclotomicPolynomial:
+    def __init__(self):
+        self.exponent = None
+        self.coefficient = None
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from itertools import count, chain
+def isprime(n):
+    for p in primes():
+        if n % p == 0:
+            return False
+        if p * p > n:
+            return True
 
+
+def primes(_cache=[2, 3]):
+    yield from _cache
+    for n in count(_cache[-1] + 2, 2):
+        if isprime(n):
+            _cache.append(n)
+            yield n
+
+for k in primes():
+    print(k)
